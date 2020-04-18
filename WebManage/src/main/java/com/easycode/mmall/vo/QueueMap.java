1c7425085ef92c4cb12bb1a89a60b7643ce4c9b1
leipengfei <1196327324@qq.com>
2020-04-18 19:51:46 +0800

    <新功能>Web<主题>  issue  添加前端相关静态文件(2)

---
+++ WebManage/src/main/java/com/easycode/mmall/vo/QueueMap.java
@@ -0,0 +1,20 @@
+package com.easycode.mmall.vo;
+
+import com.easycode.mmall.model.User;
+import com.easycode.mmall.service.UserService;
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description: //TODO
+ * @Date: 2020/4/17 17:51
+ * @Author: pengfei.L
+ */
+
+public class QueueMap {
+  public  static  List<User>  userList;
+}

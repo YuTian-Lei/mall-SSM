bf9d718a6bcfffa3078c820394bb7ff4782cbf3b
pengfei.L <leipengfei@fang.com>
2020-11-16 11:09:52 +0800

    线程池探究

--- WebManage/src/test/java/WebManageTest.java
+++ WebManage/src/test/java/WebManageTest.java
@@ -3,6 +3,7 @@ import cn.hutool.core.io.FileUtil;
 import com.easycode.mmall.async.AsyncManager;
 import com.easycode.mmall.async.AsyncProcessor;
 import com.easycode.mmall.utils.TimerManager;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -11,172 +12,183 @@ import java.io.OutputStream;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 /*@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-service.xml"})*/
 public class WebManageTest {
-  @Test
-  public void testDate() throws InterruptedException {
-    Boolean res = true;
-    CountDownLatch count = new CountDownLatch(20);
-    TimerManager.getInstance(1).execute(new TimerTask() {
-      @Override public void run() {
-        System.out.println(Thread.currentThread().getName() + "定时任务当前执行时间:" + DateUtil.now());
-        count.countDown();
-      }
-    }, TimeUnit.SECONDS, 5L);
+    @Test
+    public void testDate() throws InterruptedException {
+        Boolean res = true;
+        CountDownLatch count = new CountDownLatch(20);
+        TimerManager.getInstance(1).execute(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "定时任务当前执行时间:" + DateUtil.now());
+                count.countDown();
+            }
+        }, TimeUnit.SECONDS, 5L);
 
-    TimerManager.getInstance(2).execute(new TimerTask() {
-      @Override public void run() {
-        System.out.println(Thread.currentThread().getName() + "定时任务当前执行时间:" + DateUtil.now());
-        count.countDown();
-      }
-    }, TimeUnit.SECONDS, 3L);
+        TimerManager.getInstance(2).execute(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "定时任务当前执行时间:" + DateUtil.now());
+                count.countDown();
+            }
+        }, TimeUnit.SECONDS, 3L);
 
-    while (res) {
-      System.out.println(Thread.currentThread().getName() + "主线程当前执行时间:" + DateUtil.now());
-      Thread.sleep(20300);
-      TimerManager.getInstance(1).cancel();
-      System.out.println(Thread.currentThread().getName() + "主线程取消执行时间:" + DateUtil.now());
-      res = false;
-    }
-    count.await();
-  }
-
-  @Test
-  public void testBigDecimal() {
-    System.out.println(0.05 + 0.01);
-  }
-
-  @Test
-  public void testAysnc() {
-    while (true) {
-      AsyncManager.instance().execute(() -> {
-        System.out.println("测试线程" + DateUtil.now());
-        throw new NullPointerException();
-      });
-      try {
-        Thread.sleep(2000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-  @Test
-  public void testInterrupt(){
-    CountDownLatch countDownLatch = new CountDownLatch(1);
-    //阻塞线程
-    Thread thread1 = new Thread(()->{
-      try {
-        TimeUnit.SECONDS.sleep(5);
-      } catch (InterruptedException e) {
-        System.out.println(Thread.currentThread().getName() + "当前中断状态:"+ Thread.currentThread().isInterrupted());
-      }
-    });
-
-    //非阻塞线程
-    Thread thread2 = new Thread(()->{
-      while(!Thread.interrupted()){
-        System.out.println(Thread.currentThread().getName() + "当前中断状态:" +Thread.currentThread().isInterrupted());
-      }
-      System.out.println(Thread.currentThread().getName() + "已中断");
-      System.out.println(Thread.currentThread().getName() + "当前中断状态:" +Thread.currentThread().isInterrupted());
-    });
-
-    try {
-      thread1.start();
-      thread2.start();
-      thread1.interrupt();
-      TimeUnit.SECONDS.sleep(1);
-      thread2.interrupt();
-      countDownLatch.await();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
-
-
-  @Test
-  public void testOutFile(){
-
-    try {
-      File file = new File("D:\\ojbk\\test.txt");
-     if(!FileUtil.exist(file)){
-        FileUtil.mkParentDirs(file);
-        file.createNewFile();
-      }
-      OutputStream out = new FileOutputStream(file);
-      out.write("我爱你".getBytes("UTF-8"));
-      out.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Test
-  public void  testInFile(){
-     InputStream in = this.getClass().getClassLoader().getResourceAsStream("zfbinfo.properties");
-  }
-
-  @Test
-  public void testConcurrentHashMap(){
-    ConcurrentHashMap<Integer,String> map = new ConcurrentHashMap<>();
-
-    AsyncManager.instance().execute(()->{
-       int i = 0;
-       while (true){
-         map.put(1,Thread.currentThread().getName() + "    "+ i);
-         i++;
-         try {
-           TimeUnit.SECONDS.sleep(1);
-         } catch (InterruptedException e) {
-           e.printStackTrace();
-         }
-       }
-    });
-
-    AsyncManager.instance().execute(()->{
-      int i = 0;
-      while (true){
-        map.put(1,Thread.currentThread().getName() + "    "+ i);
-        i++;
-        try {
-          TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
+        while (res) {
+            System.out.println(Thread.currentThread().getName() + "主线程当前执行时间:" + DateUtil.now());
+            Thread.sleep(20300);
+            TimerManager.getInstance(1).cancel();
+            System.out.println(Thread.currentThread().getName() + "主线程取消执行时间:" + DateUtil.now());
+            res = false;
         }
-      }
-    });
-
-    while (true){
-      System.out.println(map.get(1));
+        count.await();
     }
 
+    @Test
+    public void testBigDecimal() {
+        System.out.println(0.05 + 0.01);
+    }
 
-  }
-
-
-
-  @Test
-  public void runThread() throws InterruptedException {
-     CountDownLatch cout = new CountDownLatch(1);
-    for(int i = 0; i < 540; i++){
-      int finalI = i;
-      AsyncProcessor.instance().execute(()->{
-        try {
-          System.out.println("线程id:"+ finalI + "开始执行");
-          TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
+    @Test
+    public void testAysnc() {
+        while (true) {
+            AsyncManager.instance().execute(() -> {
+                System.out.println("测试线程" + DateUtil.now());
+                throw new NullPointerException();
+            });
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println("线程id:"+ finalI + "执行完毕");
-      });
     }
-    cout.await();
-  }
+
+    @Test
+    public void testInterrupt() {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        //阻塞线程
+        Thread thread1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + "当前中断状态:" + Thread.currentThread().isInterrupted());
+            }
+        });
+
+        //非阻塞线程
+        Thread thread2 = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                System.out.println(Thread.currentThread().getName() + "当前中断状态:" + Thread.currentThread().isInterrupted());
+            }
+            System.out.println(Thread.currentThread().getName() + "已中断");
+            System.out.println(Thread.currentThread().getName() + "当前中断状态:" + Thread.currentThread().isInterrupted());
+        });
+
+        try {
+            thread1.start();
+            thread2.start();
+            thread1.interrupt();
+            TimeUnit.SECONDS.sleep(1);
+            thread2.interrupt();
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void testOutFile() {
+
+        try {
+            File file = new File("D:\\ojbk\\test.txt");
+            if (!FileUtil.exist(file)) {
+                FileUtil.mkParentDirs(file);
+                file.createNewFile();
+            }
+            OutputStream out = new FileOutputStream(file);
+            out.write("我爱你".getBytes("UTF-8"));
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInFile() {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("zfbinfo.properties");
+    }
+
+    @Test
+    public void testConcurrentHashMap() {
+        ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<>();
+
+        AsyncManager.instance().execute(() -> {
+            int i = 0;
+            while (true) {
+                map.put(1, Thread.currentThread().getName() + "    " + i);
+                i++;
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        AsyncManager.instance().execute(() -> {
+            int i = 0;
+            while (true) {
+                map.put(1, Thread.currentThread().getName() + "    " + i);
+                i++;
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        while (true) {
+            System.out.println(map.get(1));
+        }
+
+
+    }
+
+
+    @Test
+    public void runThread() throws InterruptedException {
+        CountDownLatch cout = new CountDownLatch(1);
+        for (int i = 1; i < 550; i++) {
+            int finalI = i;
+            AsyncProcessor.instance().execute(() -> {
+                try {
+                    System.out.println("线程id:" + finalI + "开始执行" + DateTime.now());
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("线程id:" + finalI + "执行完毕————————————————————————————————————");
+            });
+        }
+        cout.await();
+        Executors.newCachedThreadPool();
+    }
+
+    @Test
+    public void testPoolSize() throws InterruptedException {
+        System.out.println(AsyncProcessor.instance().getPoolSize());
+        TimeUnit.SECONDS.sleep(20);
+        System.out.println(AsyncProcessor.instance().getPoolSize());
+    }
 }
--- common/src/main/java/com/easycode/mmall/async/AsyncProcessor.java
+++ common/src/main/java/com/easycode/mmall/async/AsyncProcessor.java
@@ -9,6 +9,7 @@ import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
@@ -20,117 +21,133 @@ import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 @Slf4j
 public class AsyncProcessor {
 
-  /**
-   * 线程池核心池的大小<corePoolSize>  -- 计算密集型
-   * 线程数 = CPU核数+1，也可以设置成CPU核数*2，但还要看JDK的版本以及CPU配置(服务器的CPU有超线程)
-   */
-  /*private static final int COREPOOLSIZE = Runtime.getRuntime().availableProcessors() * 2;*/
+    /**
+     * 线程池核心池的大小<corePoolSize>  -- 计算密集型
+     * 线程数 = CPU核数+1，也可以设置成CPU核数*2，但还要看JDK的版本以及CPU配置(服务器的CPU有超线程)
+     */
+    /*private static final int COREPOOLSIZE = Runtime.getRuntime().availableProcessors() * 2;*/
 
-  /**
-   * 线程池核心池的大小<corePoolSize>  -- IO密集型 计算公式：线程数 = CPU核心数/(1-阻塞系数) 这个阻塞系数一般为0.8~0.9之间，也可以取0.8或者0.9
-   */
-  private static final int COREPOOLSIZE = Runtime.getRuntime().availableProcessors() * 10;
+    /**
+     * 线程池核心池的大小<corePoolSize>  -- IO密集型 计算公式：线程数 = CPU核心数/(1-阻塞系数) 这个阻塞系数一般为0.8~0.9之间，也可以取0.8或者0.9
+     */
+    private static final int COREPOOLSIZE = Runtime.getRuntime().availableProcessors() * 10;
 
-  /**
-   * 线程池的最大线程数<maximumPoolSize>
-   */
-  private static final int MAXIMUMPOOLSIZE = COREPOOLSIZE * 2;
+    /**
+     * 线程池的最大线程数<maximumPoolSize>
+     */
+    private static final int MAXIMUMPOOLSIZE = COREPOOLSIZE * 2;
 
-  /**
-   * 默认线程存活时间
-   */
-  private static final long DEFAULT_KEEP_ALIVE = 60L;
+    /**
+     * 默认线程存活时间
+     */
+    private static final long DEFAULT_KEEP_ALIVE = 60L;
 
-  /**
-   * 默认队列大小
-   */
-  private static final int DEFAULT_SIZE = 500;
+    /**
+     * 默认队列大小
+     */
+    private static final int DEFAULT_SIZE = 500;
 
-  /**
-   * 线程池名称格式
-   */
-  private static final String THREAD_POOL_NAME = "SchedulePoolThreadExecutor-%d";
+    /**
+     * 线程池名称格式
+     */
+    private static final String THREAD_POOL_NAME = "SchedulePoolThreadExecutor-%d";
 
-  /**
-   * 捕获线程异常
-   * */
-  private static Thread.UncaughtExceptionHandler uncaughtExceptionHandler = new Thread.UncaughtExceptionHandler(){
-    @Override public void uncaughtException(Thread t, Throwable e) {
-      if (e != null) {
-        log.error(e.getMessage(), e);
-      }
+    /**
+     * 捕获线程异常
+     */
+    private static Thread.UncaughtExceptionHandler uncaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            if (e != null) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    };
+
+    /**
+     * 线程工厂
+     */
+    private static final ThreadFactory FACTORY =
+            new BasicThreadFactory.Builder().namingPattern(THREAD_POOL_NAME).uncaughtExceptionHandler(uncaughtExceptionHandler).build();
+
+    /**
+     * 执行队列
+     */
+    private static BlockingQueue<Runnable> executeQueue = new ArrayBlockingQueue<>(DEFAULT_SIZE);
+
+    /**
+     * 自定义拒绝策略
+     */
+    private static RejectedExecutionHandler rejectedExecutionHandler = new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            // todo 记录异常
+            // todo 报警处理等
+            log.error("线程池拒绝策略触发!当前执行任务数:{}", executor.getActiveCount());
+        }
+    };
+
+    /**
+     * 单例模式 -- 饿汉
+     */
+    private static AsyncProcessor instance = new AsyncProcessor();
+
+    private AsyncProcessor() {
     }
-  };
 
-  /**
-   * 线程工厂
-   */
-  private static final ThreadFactory FACTORY =
-      new BasicThreadFactory.Builder().namingPattern(THREAD_POOL_NAME).uncaughtExceptionHandler(uncaughtExceptionHandler).build();
-
-  /**
-   * 执行队列
-   */
-  private static BlockingQueue<Runnable> executeQueue = new ArrayBlockingQueue<>(DEFAULT_SIZE);
-
-  /**
-   * 自定义拒绝策略
-   */
-  private static RejectedExecutionHandler rejectedExecutionHandler = new RejectedExecutionHandler(){
-    @Override public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-      // todo 记录异常
-      // todo 报警处理等
-      log.error("线程池拒绝策略触发!当前执行任务数:{}" , executor.getActiveCount());
+    public static AsyncProcessor instance() {
+        return instance;
     }
-  };
 
-  /**
-   * 单例模式 -- 饿汉
-   */
-  private static AsyncProcessor instance = new AsyncProcessor();
+    /**
+     * 异步操作任务调度线程池
+     */
+    private ExecutorService executor = new ThreadPoolExecutor(
+            COREPOOLSIZE,
+            MAXIMUMPOOLSIZE,
+            5,
+            TimeUnit.SECONDS,
+            executeQueue,
+            FACTORY,
+            rejectedExecutionHandler);
 
-  private AsyncProcessor() {
-  }
-
-  public static AsyncProcessor instance() {
-    return instance;
-  }
-
-  /**
-   * 异步操作任务调度线程池
-   */
-  private ExecutorService executor = new ThreadPoolExecutor(
-      COREPOOLSIZE,
-      MAXIMUMPOOLSIZE,
-      DEFAULT_KEEP_ALIVE,
-      TimeUnit.SECONDS,
-      executeQueue,
-      FACTORY,
-      rejectedExecutionHandler);
-
-  /**
-   * 执行任务
-   */
-  public void execute(Runnable task) {
-    executor.execute(task);
-  }
-
-  /**
-   * 提交任务
-   */
-  public <T> Future<T> submit(Callable<T> task) {
-    return executor.submit(task);
-  }
-
-  /**
-   * 停止线程池
-   */
-  public void shutdown() {
-    if(executor != null)
-    {
-      executor.shutdown();
+    //allowCoreThreadTimeOut = false 核心线程一直存在,最大线程超时会自动销毁
+    //allowCoreThreadTimeOut = true  核心线程超时也会销毁
+    public void setAllowCoreThreadTimeout(boolean value) {
+        if (null != executor) {
+            ((ThreadPoolExecutor) executor).allowCoreThreadTimeOut(value);
+        }
     }
-  }
+
+    public int getPoolSize() {
+        if (null != executor) {
+            return ((ThreadPoolExecutor) executor).getPoolSize();
+        }
+        return -1;
+    }
+
+    /**
+     * 执行任务
+     */
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+
+    /**
+     * 提交任务
+     */
+    public <T> Future<T> submit(Callable<T> task) {
+        return executor.submit(task);
+    }
+
+    /**
+     * 停止线程池
+     */
+    public void shutdown() {
+        if (executor != null) {
+            executor.shutdown();
+        }
+    }
 }
 
 
